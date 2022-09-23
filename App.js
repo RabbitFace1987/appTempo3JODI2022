aaeebca63178b093bcc52891855beb16390d3f6b
RabbitFace1987 <thiago.kardec.2004@gmail.com>
2022-09-23 10:52:51 -0300

    Adicionando Mudança de Cidade

--- App.js
+++ App.js
@@ -1,31 +1,71 @@
-import { React } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, TouchableOpacityBase } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import Tempo from './components/Tempo';
+import Api from './components/Api';
 
 export default function App() {
+  const [dados,setDados] = useState("");
+  const [cidade, setCidade] = useState("");
+
+  async function carregaTempo(){
+    const response = await Api.get(`weather?array_limit=1&fields=only_results,temp,city_name,forecast,max,min,date,description&key=1e2e36cc&city_name=${cidade},SP`);
+    setDados(response.data.forecast[0]);
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.bloco}>
-      <Text style={styles.titulo}>Tempo Divertido com o Sun</Text>
-      </View>
-      <View style={styles.bloco}>
-        <Text style={styles.label}>Cidade: </Text>
-      <TextInput
-      placeholder='Hey amiguinho, diga sua cidade para descobrir o clima!'
-      style={styles.input}
-      />
+        <View style={styles.bloco}>
+          <Text style={styles.titulo}>Tempo Divertido com o Sun</Text>
+        </View>
+        <View style={styles.bloco}>
+          <Text style={styles.label}>Cidade: </Text>
+          <TextInput 
+            placeholder='Hey amiguinho, escolha uma cidade para descobrir o clima!'
+            style={styles.input}
+            value={cidade}
+            onChangeText={(value)=>setCidade(value)}
+          />
+        </View>
+        <View style={styles.bloco}>
+            <TouchableOpacity style={styles.botao} onPress={carregaTempo}>
+                <Text style={styles.textoBotao}>Buscar</Text>
+            </TouchableOpacity>
+        </View>
+        <View style={styles.bloco}>
+          <Tempo previsao={dados}/>
+        </View>
     </View>
-    <View style={styles.bloco}>
-      <TouchableOpacity style={styles.bloco}>
-        <Text style={styles.textoBotao}>Buscar</Text>
-      </TouchableOpacity>
-    </View>
-  </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 30
   },
+  bloco:{
+    marginTop:30,
+    width:'80%',
+    marginLeft:'10%'
+  },
+  label:{
+    fontSize:20
+  },
+  titulo:{
+    fontSize:30,
+    textAlign:'center'
+  },
+  input:{
+    borderBottomWidth:2,
+    fontSize:20
+  },
+  botao:{
+    backgroundColor:"#000",
+  },
+  textoBotao:{
+    fontSize:20,
+    color:"#FFF",
+    textAlign:'center'
+  }
 });
+
+//https://api.hgbrasil.com/weather?array_limit=1&fields=only_results,temp,city_name,forecast,max,min,date,description&key=4daebe67&city_name=Peruibe,SP
